e455806e49a4c1818bf8a528eaa29857d1612756
Dipadana Putu <dipadanadev@gmail.com>
2023-08-26 20:00:07 +0800

    Fix bug

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -19,10 +19,10 @@ class CategoryController {
     } catch (err) {
       if (err.code === "P2002") {
         return res
-          .code(400)
+          .status(400)
           .json({ message: "A category with this name is already in use" });
       } else {
-        return res.code(400).json({ message: "Something error" });
+        return res.status(400).json({ message: "Something error", err });
       }
     }
   }
@@ -41,23 +41,28 @@ class CategoryController {
       res.status(200).json(result);
     } catch (err) {
       if (err.code === "P2002") {
-        return res
-          .code(400)
-          .json({ message: "A category with this name is already in use" });
+        return res.status(400).json({
+          message: "A category with this name is already in use",
+          err,
+        });
       } else {
-        return res.code(400).json({ message: "Something error" });
+        return res.status(400).json({ message: "Something error", err });
       }
     }
   }
 
   static async deleteCategory(req, res) {
-    const result = await prisma.category.delete({
-      where: {
-        id: Number(req.params.id),
-      },
-    });
+    try {
+      const result = await prisma.category.delete({
+        where: {
+          id: Number(req.params.id),
+        },
+      });
 
-    res.status(200).json(result);
+      res.status(200).json(result);
+    } catch (err) {
+      return res.status(400).json({ message: "Something error", err });
+    }
   }
 }
 
--- controllers/shoe.controller.js
+++ controllers/shoe.controller.js
@@ -8,61 +8,81 @@ class ShoeController {
   }
 
   static async getDetailShoe(req, res) {
-    const result = await prisma.shoe.findUnique({
-      where: { id: Number(req.params.id) },
-    });
+    try {
+      const result = await prisma.shoe.findUnique({
+        where: { id: Number(req.params.id) },
+        include: { category: true },
+      });
 
-    if (result === null) {
-      res.status(404).json({ msg: "Data not found" });
-    } else {
-      res.status(200).json(result);
+      if (result === null) {
+        res.status(404).json({ msg: "Data not found" });
+      } else {
+        res.status(200).json(result);
+      }
+    } catch (err) {
+      return res.status(400).json({ message: "Something error", err });
     }
   }
 
   static async addShoe(req, res) {
-    const result = await prisma.shoe.create({
-      data: {
-        name: req.body.name,
-        merk: req.body.merk,
-        desc: req.body.desc,
-        price: Number(req.body.price),
-        qty: Number(req.body.qty),
-        available:
-          (req.body.available === "true") | req.body.available ? true : false,
-      },
-    });
-    res.status(201).json(result);
+    try {
+      const result = await prisma.shoe.create({
+        data: {
+          name: req.body.name,
+          merk: req.body.merk,
+          desc: req.body.desc,
+          categoryId: Number(req.body.categoryId),
+          price: Number(req.body.price),
+          qty: Number(req.body.qty),
+          available:
+            (req.body.available === "true") | req.body.available ? true : false,
+        },
+      });
+      res.status(201).json(result);
+    } catch (err) {
+      return res.status(400).json({ message: "Something error", err });
+    }
   }
 
   static async editShoe(req, res) {
-    console.log(req.body);
-    const result = await prisma.shoe.update({
-      where: {
-        id: Number(req.params.id),
-      },
-      data: {
-        name: req.body.name ?? undefined,
-        merk: req.body.merk ?? undefined,
-        desc: req.body.desc ?? undefined,
-        price: req.body.price ? Number(req.body.price) : undefined,
-        qty: req.body.qty ? Number(req.body.qty) : undefined,
-        available: req.body.available
-          ? (req.body.available === "true") | req.body.available
-            ? true
-            : false
-          : undefined,
-      },
-    });
-    res.status(200).json(result);
+    try {
+      const result = await prisma.shoe.update({
+        where: {
+          id: Number(req.params.id),
+        },
+        data: {
+          name: req.body.name ?? undefined,
+          merk: req.body.merk ?? undefined,
+          desc: req.body.desc ?? undefined,
+          categoryId: req.body.categoryId
+            ? Number(req.body.categoryId)
+            : undefined,
+          price: req.body.price ? Number(req.body.price) : undefined,
+          qty: req.body.qty ? Number(req.body.qty) : undefined,
+          available: req.body.available
+            ? (req.body.available === "true") | req.body.available
+              ? true
+              : false
+            : undefined,
+        },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      return res.status(400).json({ message: "Something error", err });
+    }
   }
 
   static async deleteShoe(req, res) {
-    const result = await prisma.shoe.delete({
-      where: {
-        id: Number(req.params.id),
-      },
-    });
-    res.status(200).json(result);
+    try {
+      const result = await prisma.shoe.delete({
+        where: {
+          id: Number(req.params.id),
+        },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      return res.status(400).json({ message: "Something error", err });
+    }
   }
 }
 
